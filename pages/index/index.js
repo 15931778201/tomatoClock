bfa5d1dad79e0d0aac0a0c66a3e1aa54081a5f73
15931778201 <719791415@qq.com>
2021-04-16 00:18:39 +0800

    时钟倒计时

--- pages/index/index.js
+++ pages/index/index.js
@@ -49,31 +49,132 @@ Page({
     })
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+    var res = wx.getSystemInfoSync();
+    var rate = 750 / res.windowWidth;
+    this.setData({
+      rate: rate,
+      clockHeight:rate * res.windowHeight
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+  slideChange:function(e){
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      time:e.detail.value
+    })
+  },
+  // 点击事项 文字高亮
+  clickCate:function(e){
+    this.setData({
+      cateActive:e.currentTarget.dataset.item
+    })
+  },
+  // 开始专注
+  start:function(){
+    this.setData({
+      clockShow:true,
+      mTime:this.data.time*60*1000,
+      timeStr:parseInt(this.data.time) >= 10 ? this.data.time+':00' : 
+      '0' + this.data.time+':00'
+    })
+    this.drawBg()
+    this.drawActive()
+  },
+  // 画圆
+  drawBg:function(){
+    var lineWidth = 6 / this.data.rate // px
+    var ctx = wx.createCanvasContext('progress_bg')
+    ctx.setLineWidth(lineWidth)
+    ctx.setStrokeStyle('#000000');
+    ctx.setLineCap('round');
+    ctx.beginPath();
+    ctx.arc(400/this.data.rate/2,400/this.data.rate/2,400/this.data.rate/2-2*lineWidth,0,2*Math.PI,false);
+    ctx.stroke();
+    ctx.draw();
+  }, 
+  // 动态画圆
+  drawActive:function(){
+    var _this = this;
+    var timer = setInterval(function() {
+      //1.5-3.5
+      var angle = 1.5 + 2*(_this.data.time*60*1000 - _this.data.mTime)/
+      (_this.data.time*60*1000);
+      var currentTime = _this.data.mTime - 100;
+      _this.setData({
+        mTime:currentTime
+      });
+      if(angle < 3.5){
+        if(currentTime % 1000 == 0){
+          var timeStr1 = currentTime / 1000;// s
+          var timeStr2 = parseInt(timeStr1 / 60);// m
+          var timeStr3 = (timeStr1 - timeStr2*60) >= 10 ? (timeStr1 - timeStr2*60) :
+          '0'+(timeStr1 - timeStr2*60);
+          var timeStr2 = timeStr2 >= 10 ? timeStr2 : '0'+timeStr2;
+          _this.setData({
+            timeStr:timeStr2+':'+timeStr3
+          })
+        }
+        var lineWidth = 6 / _this.data.rate;//px
+        var ctx = wx.createCanvasContext('progress_active');
+        ctx.setLineWidth(lineWidth);
+        ctx.setStrokeStyle('#ffffff');
+        ctx.setLineCap('round');
+        ctx.beginPath();
+        ctx.arc(400/_this.data.rate/2,400/_this.data.rate/2,400/_this.data.rate/2-2*lineWidth,
+          1.5*Math.PI,angle*Math.PI,false);
+        ctx.stroke();
+        ctx.draw();
+      }else{
+        var logs = wx.getStorageSync('logs') || [];
+        logs.unshift({
+          date:util.formatTime(new Date),
+          cate:_this.data.cateActive,
+          time:_this.data.time
+        });
+        wx.setStorageSync('logs', logs);
+        _this.setData({
+          timeStr:'00:00',
+          okShow:true,
+          pauseShow:false,
+          continueCancelShow:false
+        });
+        clearInterval(timer);
+      }
+    },100)
+    _this.setData({
+      timer:timer
+    })
+  },
+  pause:function(){
+    clearInterval(this.data.timer);
+    this.setData({
+      pauseShow:false,
+      continueCancelShow:true,
+      okShow:false
+    })
+  },
+  continue:function(){
+    this.drawActive();
+    this.setData({
+      pauseShow:true,
+      continueCancelShow:false,
+      okShow:false
+    })
+  },
+  cancel:function(){
+    clearInterval(this.data.timer);
+    this.setData({
+      pauseShow:true,
+      continueCancelShow:false,
+      okShow:false,
+      clockShow:false
+    })
+  },
+  ok:function(){
+    clearInterval(this.data.timer);
+    this.setData({
+      pauseShow:true,
+      continueCancelShow:false,
+      okShow:false,
+      clockShow:false
     })
   }
 })
